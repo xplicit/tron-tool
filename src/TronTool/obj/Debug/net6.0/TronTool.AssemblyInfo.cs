732acbc9925e359c0f53d5841174d6b84c7486fd
alexey.shevin <alexey.shevin@emu.codes>
2024-04-04 16:18:02 +0700

    update

--- src/TronTool/obj/Debug/net6.0/TronTool.AssemblyInfo.cs
+++ src/TronTool/obj/Debug/net6.0/TronTool.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tron")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ea63a4e832954642b9ff8d19c177ba586f575bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("tron")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tron")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
